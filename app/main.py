d59693071ee0fdc3590d1613e09c1905777c19c4
Diogo Alves <diogoalves@Diogos-Air.Home>
2024-09-06 23:45:17 +0100

    fix buffer size and refresh rate

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.include_router(app_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Or specify your domain(s) here
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
